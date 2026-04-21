23cbc0e5953eb70f154367476e7f0c8c874cd520
Kamasani5356 <83870937+Kamasani5356@users.noreply.github.com>
2026-04-21 22:02:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,36 @@
+resource "google_alloydb_instance" "primary" {
+  project     = var.project_id
+  cluster     = google_alloydb_cluster.cluster.name
+  instance_id = "${var.cluster_id}-primary"
+  instance_type = "PRIMARY"
+
+  display_name = "${var.cluster_display_name} Primary"
+
+  machine_config {
+    cpu_count = var.primary_cpu_count
+  }
+
+  database_flags = var.database_flags
+
+  labels = var.cluster_labels
+}
+
+resource "google_alloydb_instance" "read_pool" {
+  count       = var.enable_read_pool ? 1 : 0
+  project     = var.project_id
+  cluster     = google_alloydb_cluster.cluster.name
+  instance_id = "${var.cluster_id}-rp-1"
+  instance_type = "READ_POOL"
+
+  display_name = "${var.cluster_display_name} Read Pool"
+
+  read_pool_config {
+    node_count = var.read_pool_node_count
+  }
+
+  depends_on = [google_alloydb_instance.primary]
+}
+
 resource "google_alloydb_cluster" "cluster" {
   project  = var.project_id
   cluster_id = var.cluster_id
